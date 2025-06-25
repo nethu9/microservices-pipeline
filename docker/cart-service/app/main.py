de0cff8e28d5cc3f40b05cfb5a249fbb326f3899
Nethu Yahampath <89215068+nethu9@users.noreply.github.com>
2025-06-25 20:17:47 +0300

    Update main.py

--- docker/cart-service/app/main.py
+++ docker/cart-service/app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/cart")
+@app.get("/carts")
 def read_cart():
-    return {"cart": ["item1", "item2"]}
+    return {"cart": ["item1", "item2"}
